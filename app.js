2b317eeb2136350a0b17d738d22112ec1027c627
Rod-S <rodsanati@gmail.com>
2021-05-11 15:39:50 -0500

    add props to components

--- app.js
+++ app.js
@@ -4,36 +4,55 @@
 // const myTitleID = 'main-title';
 // const name = 'Rod';
 
-const Header = () => (
+const Header = (props) => {
+    console.log(props);
+    return (
         <header>
             {/*static placeholder text*/}
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{props.title}</h1>
+            <span className="stats">Players: {props.totalPlayers(5)}</span>
         </header>
-    );
+    )};
 
-const Player = () => (
+const Player = (props) => (
     <div className="player">
         <span className="player-name">
-            Rod
+            {props.name}
         </span>
-        <Counter />
+        <Counter score={props.score}/>
     </div>
 );
 
-const Counter = () => (
+const Counter = (props) => (
     <div className="counter">
         <button className="counter-action decrement"> - </button>
-        <span className="counter-score">0</span>
+        <span className="counter-score">{props.score}</span>
         <button className="counter-action increment"> + </button>
     </div>
 );
 
 const App = () => (
     <div className="scoreboard">
-        <Header />
-        {/*Players list*/}
-        <Player />
+        <Header
+            title="Scoreboard"
+            totalPlayers={n => n + 10} />
+            {/*Players List*/}
+        <Player
+            name="Rod"
+            score={50}
+        />
+        <Player
+            name="James"
+            score={20}
+        />
+        <Player
+            name="John"
+            score={30}
+        />
+        <Player
+            name="Joe"
+            score={10}
+        />
     </div>
 );
 
