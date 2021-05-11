6ff0358a4a03d1a3ea6c86a329f8fe4691b10893
Rod-S <rodsanati@gmail.com>
2021-05-10 23:54:38 -0500

    create jsx expressions

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 //object representing what you want rendered to the DOM
-const title = <h1>My first React Element!</h1>;
+const title = "My React Element!";
+const desc = "I created a React node and rendered it into the DOM.";
+const myTitleID = 'main-title';
+const name = 'Rod';
 
-const desc = <p>I just learned how to create a React node and render it into the DOM.</p>
-
-const header = React.createElement(
-    'header',
-    null,
-    title,
-    desc
+const header = (
+    <header>
+        {/*comment*/}
+        <h1 id={myTitleID}>{name}'s React Element!</h1>
+        <p className="main-desc">{desc}</p>
+    </header>
 )
 
 ReactDOM.render(
