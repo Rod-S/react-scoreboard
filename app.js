1e550e623225bbdb8184ab72520b9f180fe715ff
Rod-S <rodsanati@gmail.com>
2021-05-11 01:24:26 -0500

    create Player component

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 //object representing what you want rendered to the DOM
-const title = "My React Element!";
-const desc = "I created a React node and rendered it into the DOM.";
-const myTitleID = 'main-title';
-const name = 'Rod';
+// const title = "My React Element!";
+// const desc = "I created a React node and rendered it into the DOM.";
+// const myTitleID = 'main-title';
+// const name = 'Rod';
 
-const header = (
-    <header>
-        {/*comment*/}
-        <h1 id={myTitleID}>{name}'s React Element!</h1>
-        <p className="main-desc">{desc}</p>
-    </header>
-)
+const Header = () => (
+        <header>
+            {/*static placeholder text*/}
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+
+const Player = () => (
+    <div className="player">
+        <span className="player-name">
+            Rod
+        </span>
+        <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <span className="counter-score">0</span>
+                <button className="counter-action increment"> + </button>
+            </div>
+    </div>
+);
+
 
 ReactDOM.render(
-    header,
+    <Player />,
     document.getElementById('root')
 )
