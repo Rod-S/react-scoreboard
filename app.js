0c3524c47703f3e31b5bd3350956c235ad5ff609
Rod-S <rodsanati@gmail.com>
2021-05-11 21:50:44 -0500

    add players data and iterate to render multiple player components

--- app.js
+++ app.js
@@ -4,13 +4,36 @@
 // const myTitleID = 'main-title';
 // const name = 'Rod';
 
+const players = [
+    {
+        name: "Guil",
+        score: 50,
+        id: 0
+    },
+    {
+        name: "Treasure",
+        score: 85,
+        id: 1
+    },
+    {
+        name: "Ashley",
+        score: 95,
+        id: 2
+    },
+    {
+        name: "James",
+        score: 80,
+        id: 3
+    }
+];
+
 const Header = (props) => {
     console.log(props);
     return (
         <header>
             {/*static placeholder text*/}
             <h1>{props.title}</h1>
-            <span className="stats">Players: {props.totalPlayers(5)}</span>
+            <span className="stats">Players: {props.totalPlayers}</span>
         </header>
     )};
 
@@ -31,32 +54,24 @@ const Counter = (props) => (
     </div>
 );
 
-const App = () => (
+const App = (props) => (
     <div className="scoreboard">
         <Header
             title="Scoreboard"
-            totalPlayers={n => n + 10} />
-            {/*Players List*/}
-        <Player
-            name="Rod"
-            score={50}
-        />
-        <Player
-            name="James"
-            score={20}
-        />
-        <Player
-            name="John"
-            score={30}
-        />
-        <Player
-            name="Joe"
-            score={10}
-        />
+            totalPlayers={props.initialPlayers.length} />
+
+        {/*Players List*/}
+        {props.initialPlayers.map(player =>
+            <Player
+                key={player.id.toString()}
+                name={player.name}
+                score={player.score}
+            />
+        )}
     </div>
 );
 
 ReactDOM.render(
-    <App />,
+    <App initialPlayers={players}/>,
     document.getElementById('root')
 )
